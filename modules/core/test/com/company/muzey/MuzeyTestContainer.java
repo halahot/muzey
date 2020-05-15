21d19b4614551b470dbbcbccaede32ad9444d3f8
Maxim Gunko <m.gunko@infinnity.ru>
2020-05-15 11:05:02 +0500

    Create project

---
+++ modules/core/test/com/company/muzey/MuzeyTestContainer.java
@@ -0,0 +1,58 @@
+package com.company.muzey;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MuzeyTestContainer extends TestContainer {
+
+    public MuzeyTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/muzey/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/muzey/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends MuzeyTestContainer {
+
+        public static final MuzeyTestContainer.Common INSTANCE = new MuzeyTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
